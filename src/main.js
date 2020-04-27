3c14df03d94560e9b7661e559bedb64ed3432d33
Francisca <franciscabelen112002@gmail.com>
2020-04-27 12:46:31 -0400

    html con menú

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import data from './data/potter/potter.js';
 
 console.log(example, data);
+
+
+function menuDesplegable() {
+    var x = document.getElementById("myMenú");
+    if (x.className === "menú") {
+      x.className += " responsive";
+    } else {
+      x.className = "menú";
+    }
+  }
+  
